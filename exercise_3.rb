a07e74c03522a5525051ce9e9d2cd91ac4f8fdb1
Pauline Rudge <paulinerudge@me.com>
2019-01-17 07:57:10 +0000

    finished last part

--- exercise_3.rb
+++ exercise_3.rb
@@ -33,17 +33,14 @@ for countries in united_kingdom
 end
 
 # Use a loop to find the total population of the UK.
-# population = 0
-# puts "we are here for total pop"
-# population=0
-#
-# united_kingdom.each do |key, array|
-#   puts "#{key}-----"
-#   puts array
-#   population +=  united_kingdom[:population]
-# end
-#
-# # puts "population total is : #{population}"
+population=0
+counter=0
+for country in united_kingdom
+  population =+ country[:population]
+end
+p "Total population is #{population}"
+
+
 
 #
 
